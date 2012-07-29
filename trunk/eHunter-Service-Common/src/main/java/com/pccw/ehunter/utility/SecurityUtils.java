26b98c0822e449aaf0cb06c9be92f16beca73f56
beck.bq.lu@gmail.com <beck.bq.lu@gmail.com@ff27eaf3-3b06-2d20-575c-d4463c8be1ab>
2012-07-29 11:00:08 +0000

    新增AbstractUserDetails，修改InternalUserDto,以增加重新统一性

---
+++ trunk/eHunter-Service-Common/src/main/java/com/pccw/ehunter/utility/SecurityUtils.java
@@ -0,0 +1,34 @@
+package com.pccw.ehunter.utility;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.pccw.ehunter.constant.CommonConstant;
+import com.pccw.ehunter.security.AbstractUserDetails;
+
+public class SecurityUtils {
+	
+	public static UserDetails getUser(){
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		
+		if(authentication != null){
+			if(authentication.getPrincipal() != null && authentication.getPrincipal() instanceof UserDetails){
+				return (UserDetails)authentication.getPrincipal();
+			}else{
+				return null;
+			}
+		}
+		
+		return null;
+	}
+	
+	public static String getUserRecId(){
+		UserDetails usr = getUser();
+		if(usr != null && usr instanceof AbstractUserDetails){
+			return ((AbstractUserDetails)usr).getUserRecId();
+		}else {
+			return CommonConstant.ANOYMOUS_INTERNAL_USER;
+		}
+	}
+}
