0ab661b15b9ab2061e56cc1d43d8c57f29b1bdad
beck.bq.lu@gmail.com <beck.bq.lu@gmail.com@ff27eaf3-3b06-2d20-575c-d4463c8be1ab>
2012-07-29 11:00:23 +0000

    新增AbstractUserDetails，修改InternalUserDto,以增加重新统一性

--- eHunter-Service-Internal/src/main/java/com/pccw/ehunter/dto/InternalUserDTO.java
+++ eHunter-Service-Internal/src/main/java/com/pccw/ehunter/dto/InternalUserDTO.java
@@ -6,115 +6,17 @@ import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.CollectionUtils;
 
 import com.pccw.ehunter.constant.CommonConstant;
+import com.pccw.ehunter.security.AbstractUserDetails;
 
-public class InternalUserDTO extends BaseDTO implements UserDetails {
+public class InternalUserDTO extends AbstractUserDetails {
 	private static final long serialVersionUID = 1L;
-
-	private String userRecId;
-	private String loginId;
-	private String password;
+	
 	private String confirmPassword;
-	private String staffId;
-	private String accountStatus;
-	private String enName;
-	private String cnName;
-	private String passwordExpired;
-	private String phone;
-	private String mobile;
-	private String email;
+
 	private List<InternalRoleDTO> activeRoles;
-
-	public String getUserRecId() {
-		return userRecId;
-	}
-
-	public void setUserRecId(String userRecId) {
-		this.userRecId = userRecId;
-	}
-
-	public String getLoginId() {
-		return loginId;
-	}
-
-	public void setLoginId(String loginId) {
-		this.loginId = loginId;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getStaffId() {
-		return staffId;
-	}
-
-	public void setStaffId(String staffId) {
-		this.staffId = staffId;
-	}
-
-	public String getAccountStatus() {
-		return accountStatus;
-	}
-
-	public void setAccountStatus(String accountStatus) {
-		this.accountStatus = accountStatus;
-	}
-
-	public String getEnName() {
-		return enName;
-	}
-
-	public void setEnName(String enName) {
-		this.enName = enName;
-	}
-
-	public String getCnName() {
-		return cnName;
-	}
-
-	public void setCnName(String cnName) {
-		this.cnName = cnName;
-	}
-
-	public String getPasswordExpired() {
-		return passwordExpired;
-	}
-
-	public void setPasswordExpired(String passwordExpired) {
-		this.passwordExpired = passwordExpired;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getMobile() {
-		return mobile;
-	}
-
-	public void setMobile(String mobile) {
-		this.mobile = mobile;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
 
 	public List<InternalRoleDTO> getActiveRoles() {
 		return activeRoles;
