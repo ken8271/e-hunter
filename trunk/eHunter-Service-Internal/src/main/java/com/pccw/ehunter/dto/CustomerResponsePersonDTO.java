a71c78db04939072efc1055c33fb23bd0d324914
beck.bq.lu@gmail.com <beck.bq.lu@gmail.com@ff27eaf3-3b06-2d20-575c-d4463c8be1ab>
2012-08-02 12:28:15 +0000

    客戶管理DTO

---
+++ trunk/eHunter-Service-Internal/src/main/java/com/pccw/ehunter/dto/CustomerResponsePersonDTO.java
@@ -0,0 +1,70 @@
+package com.pccw.ehunter.dto;
+
+public class CustomerResponsePersonDTO extends BaseDTO {
+	private static final long serialVersionUID = 1L;
+
+	private String systemRespRefNum;
+	private String name;
+	private String positionType;
+	private String positionName;
+	private MobilePhone telephone;
+	private String email;
+	private String status;
+
+	public String getSystemRespRefNum() {
+		return systemRespRefNum;
+	}
+
+	public void setSystemRespRefNum(String systemRespRefNum) {
+		this.systemRespRefNum = systemRespRefNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPositionType() {
+		return positionType;
+	}
+
+	public void setPositionType(String positionType) {
+		this.positionType = positionType;
+	}
+
+	public String getPositionName() {
+		return positionName;
+	}
+
+	public void setPositionName(String positionName) {
+		this.positionName = positionName;
+	}
+
+	public MobilePhone getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(MobilePhone telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
