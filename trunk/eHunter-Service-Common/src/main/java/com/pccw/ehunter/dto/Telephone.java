5e74dc8480afacd43d526000535c9fd9e79e4226
beck.bq.lu@gmail.com <beck.bq.lu@gmail.com@ff27eaf3-3b06-2d20-575c-d4463c8be1ab>
2012-08-02 12:27:43 +0000

    新增common固定電話/手機號碼bean

---
+++ trunk/eHunter-Service-Common/src/main/java/com/pccw/ehunter/dto/Telephone.java
@@ -0,0 +1,41 @@
+package com.pccw.ehunter.dto;
+
+import java.io.Serializable;
+
+public class Telephone implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String stateCode;
+	private String regionCode;
+	private String phoneNumber;
+
+	public String getStateCode() {
+		return stateCode;
+	}
+
+	public void setStateCode(String stateCode) {
+		this.stateCode = stateCode;
+	}
+
+	public String getRegionCode() {
+		return regionCode;
+	}
+
+	public void setRegionCode(String regionCode) {
+		this.regionCode = regionCode;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	@Override
+	public String toString() {
+		return regionCode + "-" + phoneNumber;
+	}
+
+}
