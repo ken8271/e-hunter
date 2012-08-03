a60b83644d911de9b91643f0099a09105cd5265a
beck.bq.lu@gmail.com <beck.bq.lu@gmail.com@ff27eaf3-3b06-2d20-575c-d4463c8be1ab>
2012-08-03 10:41:33 +0000

    新增BaseLabelValueDTO以适应SELECT组建

---
+++ trunk/eHunter-Service-Common/src/main/java/com/pccw/ehunter/dto/BaseLabelValueDTO.java
@@ -0,0 +1,27 @@
+package com.pccw.ehunter.dto;
+
+import java.io.Serializable;
+
+public class BaseLabelValueDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String label;
+	private String value;
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
