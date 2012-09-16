dc2f8a37d456aadf5749d16c380081e3d9d6e0d7
oklookg <oklookg@ff27eaf3-3b06-2d20-575c-d4463c8be1ab>
2012-09-16 15:56:09 +0000

    export sql test

---
+++ trunk/eHunter-Service-Common/src/test/java/com/pccw/ehunter/common/CommonTest.java
@@ -0,0 +1,30 @@
+package com.pccw.ehunter.common;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class CommonTest {
+	private final String DB_URL = "localhost";
+	private final String DB_NAME = "eHunter";
+	private final String DB_USER = "root";
+	private final String DB_PASSWORD = "oklook";
+
+	private final String EX_SQL_DISC = "D:";
+	private final String EX_SQL_DIR = "eHunter";
+	private final String EX_SQL_FILE = "eHunter.sql";
+	private final String EX_SQL_BAT_FILE = "exportEHunter.bat";
+	private final String EX_SQL_URI = EX_SQL_DISC + File.separator + EX_SQL_DIR + File.separator + EX_SQL_FILE;
+	private final String EX_SQL_BAT_URI = EX_SQL_DISC + File.separator + EX_SQL_DIR + File.separator + EX_SQL_BAT_FILE;
+	private final String EX_SQL_CMD = "mysqldump -h"+DB_URL+" -u"+DB_USER+" -p"+DB_PASSWORD+" " + DB_NAME + " > "+EX_SQL_URI;
+	
+	@Test
+	public void exportSQL(){
+		try {
+//			ExportSQLUtil.createBat(EX_SQL_BAT_URI, EX_SQL_CMD);
+			ExportSQLUtil.exportAllSql(EX_SQL_BAT_URI,EX_SQL_CMD);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
