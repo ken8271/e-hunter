4b3b8f29a9befba90eeb017d3b00552a7f986f5d
beck.bq.lu@gmail.com <beck.bq.lu@gmail.com@ff27eaf3-3b06-2d20-575c-d4463c8be1ab>
2012-12-03 04:52:54 +0000

    增加全文搜索引擎

---
+++ eHunter-Service-Internal/src/main/java/com/pccw/ehunter/constant/ContentSearchConstant.java
@@ -0,0 +1,11 @@
+package com.pccw.ehunter.constant;
+
+public class ContentSearchConstant {
+
+	public static final String FIELD_TALENT_ID = "talentid";
+	public static final String FIELD_CV_PATH = "path";
+	public static final String FIELD_CV_CONTENT  = "content";
+	
+	public static final String FUZZY_SEARCH = "F";
+	public static final String PRECISE_SEARCH = "P";
+}
