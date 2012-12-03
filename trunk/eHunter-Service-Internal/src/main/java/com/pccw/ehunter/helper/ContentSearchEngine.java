47aead512d819e4cad9f811402a7d927db986e1a
beck.bq.lu@gmail.com <beck.bq.lu@gmail.com@ff27eaf3-3b06-2d20-575c-d4463c8be1ab>
2012-12-03 04:53:08 +0000

    增加全文搜索引擎

--- trunk/eHunter-Service-Internal/src/main/java/com/pccw/ehunter/helper/ContentSearchEngine.java
+++ trunk/eHunter-Service-Internal/src/main/java/com/pccw/ehunter/helper/ContentSearchEngine.java
@@ -1,54 +1,141 @@
 package com.pccw.ehunter.helper;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import org.wltea.analyzer.lucene.IKAnalyzer;
+
+import com.pccw.ehunter.constant.ContentSearchConstant;
+import com.pccw.ehunter.dto.ContentSearchCriteria;
+import com.pccw.ehunter.utility.FileUtils;
+import com.pccw.ehunter.utility.PdfUtils;
 
 @Component("contentSearchEngine")
 public class ContentSearchEngine {
 	
-	private String indexPath;
+	private Logger logger = LoggerFactory.getLogger(ContentSearchEngine.class);
 	
-	private String srcPath;
+	private String indexPath;
 	
 	@Autowired
 	public void setProperties(Properties xmlProcessorConfig){
 		indexPath = xmlProcessorConfig.getProperty("resume.path.index");
-		srcPath = xmlProcessorConfig.getProperty("resume.path.upload");
 	}
 	
-	public void createIndex(String filePath) throws Exception{
-		File src = new File(filePath);
+	public void createIndex(String talentid , String filePath) throws Exception{
+		IndexWriter writer = null;
+		try {
+			File src = new File(filePath);
+			
+			if(!src.exists()) return ;
+			
+			initial();
+			
+			Directory dir = FSDirectory.open(new File(indexPath));
+			
+			IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_36, new IKAnalyzer());
+			iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+			
+			writer = new IndexWriter(dir, iwc);
+			
+			Document doc = new Document();
+			doc.add(new Field(ContentSearchConstant.FIELD_TALENT_ID, talentid , Field.Store.YES, Field.Index.NO));
+			doc.add(new Field(ContentSearchConstant.FIELD_CV_PATH, src.getPath() , Field.Store.YES, Field.Index.NO));
+			doc.add(new Field(ContentSearchConstant.FIELD_CV_CONTENT, getContent(filePath),Field.Store.NO, Field.Index.ANALYZED));
+			
+			writer.addDocument(doc);
+		} catch (Exception e) {
+			logger.error(">>>>>exception catched (createIndex) , error message : " + e.getMessage());
+			throw e;
+		} finally {
+			if(writer != null){
+				writer.close();
+				writer = null;
+			}
+		}
+	}
+	
+	private String getContent(String filePath) throws Exception{
+		String type = FileUtils.getExtension(filePath);
 		
-		if(!src.exists()) return ;
+		if(FileUtils.PDF_FILE_EXT.equals(type)){
+			return PdfUtils.parsePdf(filePath);
+		} else {
+			return "";
+		}
+	}
+	
+	private Query getQueryCriterias(QueryParser parser , List<ContentSearchCriteria> criterias) throws Exception{
+		BooleanQuery booleanQuery = new BooleanQuery(); 
+		
+		if(!CollectionUtils.isEmpty(criterias)){
+			for(ContentSearchCriteria c : criterias){
+				if(ContentSearchCriteria.RANGE_CRITERIA.equals(c.getType())){
+					
+				}else  if(ContentSearchCriteria.TERM_CRITERIA.equals(c.getType())){
+					logger.debug(">>>>> term criteria :" + c.getValue());
+					booleanQuery.add(new TermQuery(new Term(ContentSearchConstant.FIELD_CV_CONTENT, c.getValue())), BooleanClause.Occur.SHOULD);
+				}
+			}			
+		}
+		
+		return booleanQuery;
+	}
+	
+	public List<String> handleSearch(List<ContentSearchCriteria> criterias) throws Exception{
+		List<String> matches = new ArrayList<String>();
 		
 		initial();
 		
 		Directory dir = FSDirectory.open(new File(indexPath));
+		IndexReader reader = IndexReader.open(dir);
+		IndexSearcher searcher = new IndexSearcher(reader);
 		
-		IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_36, new StandardAnalyzer(Version.LUCENE_36));
-		iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+		Analyzer analyzer = new IKAnalyzer();
 		
-		IndexWriter writer = new IndexWriter(dir, iwc);
+		QueryParser parser = new QueryParser(Version.LUCENE_36, ContentSearchConstant.FIELD_CV_CONTENT, analyzer);
 		
-		Document doc = new Document();
-		doc.add(null);
-		
+		Query query = getQueryCriterias(parser, criterias);
+	    TopDocs topDocs = searcher.search(query, 100);
+	    
+	    ScoreDoc[] hits = topDocs.scoreDocs;  
+	    if(hits != null && hits.length != 0){
+	    	for (ScoreDoc scoreDoc : hits) {  
+	    		Document hitDoc = searcher.doc(scoreDoc.doc);  
+	    		matches.add(hitDoc.get(ContentSearchConstant.FIELD_TALENT_ID));
+	    	}	    	
+	    }
+	    
+	    return matches;
 	}
 	
-	private void initial(){
+	private  void initial(){
 		File indexDir = new File(indexPath);
 		
 		if(!indexDir.exists()){
